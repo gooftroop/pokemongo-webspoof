2e72421e17a9f3b15758aae494c312452de7abf2
Albert Liu <altliu@gmail.com>
2016-12-01 15:38:52 -0800

    loader update

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -14,9 +14,9 @@ export default {
 
   module: {
     rules: [
-      { test: /\.js$/, exclude: /node_modules/, loader: 'babel' },
-      { test: /\.css$/, loader: 'style!css!postcss' },
-      { test: /\.json$/, loader: 'json' }
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
+      { test: /\.css$/, loader: 'style-loader!css-loader!postcss-loader' },
+      { test: /\.json$/, loader: 'json-loader' }
     ]
   },
 
