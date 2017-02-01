bcad318daa40a7bc8be0bb43d561e46dcd78b53c
Albert Liu <altliu@gmail.com>
2017-01-31 17:01:41 -0800

    new shortcuts

--- src/views/map/shortcuts.js
+++ src/views/map/shortcuts.js
@@ -11,7 +11,9 @@ const shortcuts = [
   [ 'Pier 39', {lat: 37.811159, long: -122.410799}],
   [ 'Wharf', {lat: 37.810453, long: -122.417390}],
   [ 'Ferry Building', {lat: 37.795349, long: -122.392167}],
-  [ 'Union Square', {lat: 37.788355, long: -122.406944}]
+  [ 'Union Square', {lat: 37.788355, long: -122.406944}],
+  [ 'Gym Path', {lat: 37.731050, long: -122.130950}],
+  [ 'Abra Park', {lat: 37.582628, long: -122.343961}]
 ]
 
 // TODO: add randomization to destinations
