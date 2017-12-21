04350b942674d2c3ce0ef62e033b06983afa75aa
Abhimanyu Chakravarty <viperzenator@gmail.com>
2017-12-21 12:56:43 +0530

    Modified some stuff

--- src/views/map/index.js
+++ src/views/map/index.js
@@ -41,10 +41,20 @@ class Map extends Component {
 		// }
 
 		// London
-		// this.handleGeolocationSuccess({ coords: { latitude: 51.507341, longitude: -0.127654 } })
+		this.handleGeolocationSuccess(
+			{
+				coords: {
+					latitude: 51.507341,
+					longitude: -0.127654
+				}
+			}
+		);
 
 		// NYC
-		// this.handleGeolocationSuccess({ coords: { latitude: 40.764762, longitude: -73.973121 } })
+		this.handleGeolocationSuccess({ coords: { latitude: 40.764762, longitude: -73.973121 } })
+
+		// Sacremento
+		// this.handleGeolocationSuccess({ coords: { latitude: 38.387697, longitude: -121.429502 } })
 
 		// Sydney
 		// this.handleGeolocationSuccess({ coords: { latitude: -33.865935, longitude: 151.215482 } })
@@ -56,7 +66,7 @@ class Map extends Component {
 		// this.handleGeolocationSuccess({ coords: { latitude: -23.584369, longitude: -46.660948 } })
 
 		// Singapore
-		this.handleGeolocationSuccess({ coords: { latitude: 1.36067684, longitude: 103.73457338 } });
+		// this.handleGeolocationSuccess({ coords: { latitude: 1.36067684, longitude: 103.73457338 } });
 	}
 
 	// geolocation API might be down, use http://ipinfo.io
@@ -67,13 +77,43 @@ class Map extends Component {
       <strong>Error getting your geolocation, using IP location</strong>
       <div class='stack'>${geolocationErr.message}</div>
     `,
-			{ timeout: 3000 }
+			{
+				timeout: 3000
+			}
 		);
 
 		try {
-			const { data: { loc } } = await axios({ url: 'http://ipinfo.io/' });
-			const [latitude, longitude] = loc.split(',').map(coord => parseFloat(coord));
-			this.handleGeolocationSuccess({ coords: { latitude, longitude } });
+			const {
+				data: {
+					loc
+				}
+			} = await axios(
+				{
+					url:
+						'http://ipinfo.io/'
+				}
+			);
+			const [
+				latitude,
+				longitude
+			] = loc
+				.split(
+					','
+				)
+				.map(
+					coord =>
+						parseFloat(
+							coord
+						)
+				);
+			this.handleGeolocationSuccess(
+				{
+					coords: {
+						latitude,
+						longitude
+					}
+				}
+			);
 		} catch (xhrErr) {
 			Alert.error(`
         <strong>Could not use IP location</strong>
@@ -84,65 +124,155 @@ class Map extends Component {
 	};
 
 	@action
-	handleGeolocationSuccess({ coords: { latitude, longitude } }) {
-		userLocation.replace([latitude, longitude]);
+	handleGeolocationSuccess({
+		coords: {
+			latitude,
+			longitude
+		}
+	}) {
+		userLocation.replace(
+			[
+				latitude,
+				longitude
+			]
+		);
 	}
 
 	@action
 	toggleMapDrag = () => {
-		this.mapOptions.draggable = !this.mapOptions.draggable;
-		this.map.map_.setOptions(toJS(this.mapOptions));
+		this.mapOptions.draggable = !this
+			.mapOptions
+			.draggable;
+		this.map.map_.setOptions(
+			toJS(
+				this
+					.mapOptions
+			)
+		);
 	};
 
 	@action
-	handleClick = ({ lat, lng }, force) => {
-		if (!this.mapOptions.draggable || force) {
-			this.autopilot.handleSuggestionChange({ suggestion: { latlng: { lat, lng } } });
+	handleClick = (
+		{
+			lat,
+			lng
+		},
+		force
+	) => {
+		if (
+			!this
+				.mapOptions
+				.draggable ||
+			force
+		) {
+			this.autopilot.handleSuggestionChange(
+				{
+					suggestion: {
+						latlng: {
+							lat,
+							lng
+						}
+					}
+				}
+			);
 		}
 	};
 
 	render() {
-		const [latitude, longitude] = userLocation;
+		const [
+			latitude,
+			longitude
+		] = userLocation;
 
 		return (
 			<div className='google-map-container'>
 				{/* only display google map when user geolocated */}
-				{latitude && longitude ? (
+				{latitude &&
+				longitude ? (
 					<GoogleMap
 						ref={ (ref) => {
 							this.map = ref;
 						} }
 						zoom={ settings.zoom.get() }
-						center={ [latitude, longitude] }
-						onClick={ this.handleClick }
-						options={ () => this.mapOptions }
-						onGoogleApiLoaded={ this.handleGoogleMapLoaded }
+						center={ [
+							latitude,
+							longitude
+						] }
+						onClick={
+							this
+								.handleClick
+						}
+						options={ () =>
+							this
+								.mapOptions
+						}
+						onGoogleApiLoaded={
+							this
+								.handleGoogleMapLoaded
+						}
 						yesIWantToUseGoogleMapApiInternals
-						apiKey={ MapsApi.apiKey }>
+						apiKey={
+							MapsApi.apiKey
+						}>
 						{/* userlocation center */}
-						<Pokeball lat={ userLocation[0] } lng={ userLocation[1] } />
+						<Pokeball
+							lat={
+								userLocation[0]
+							}
+							lng={
+								userLocation[1]
+							}
+						/>
 					</GoogleMap>
 				) : (
 					<div
 						style={ {
-							position: 'absolute',
-							top: 'calc(50vh - (100px / 2) - 60px)',
-							left: 'calc(50vw - (260px / 2))'
+							position:
+								'absolute',
+							top:
+								'calc(50vh - (100px / 2) - 60px)',
+							left:
+								'calc(50vw - (260px / 2))'
 						} }
 						className='alert alert-info text-center'>
-						<i style={ { marginBottom: 10 } } className='fa fa-spin fa-2x fa-refresh' />
-						<div>Loading user location & map...</div>
+						<i
+							style={ {
+								marginBottom: 10
+							} }
+							className='fa fa-spin fa-2x fa-refresh'
+						/>
+						<div >
+							Loading
+							user
+							location
+							&
+							map...
+						</div>
 					</div>
 				)}
 
 				<div className='btn btn-drag-map'>
-					{this.mapOptions.draggable ? (
-						<div className='btn btn-sm btn-primary' onClick={ this.toggleMapDrag }>
-							Map draggable
+					{this
+						.mapOptions
+						.draggable ? (
+							<div
+								className='btn btn-sm btn-primary'
+								onClick={
+								this
+									.toggleMapDrag
+							}>
+							Map
+							draggable
 						</div>
 					) : (
-						<div className='btn btn-sm btn-secondary' onClick={ this.toggleMapDrag }>
-							Map locked
+						<div
+							className='btn btn-sm btn-secondary'
+							onClick={
+								this
+									.toggleMapDrag
+							}>
+							Map
+							locked
 						</div>
 					)}
 				</div>
